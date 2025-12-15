9c5e00453839e22def7fe17efe705cffd7264900
sebrai <sebbranden@gmail.com>
2025-12-15 11:17:22 +0100

    defined vars and classes

--- js/classes.js
+++ js/classes.js
@@ -1,1 +1,38 @@
+class enemy {
+    constructor(type) {
+        this.key = "key("+key_incrementor+")"
+        key_incrementor+= 1
+        // body goes here
+        this.hero = type
+        this.jason = false
+    }
+}
+class animation_que_item {
+    constructor(func,target) {
+        this.do = func
+        this.target =target
+    }
+}
+class ally {
+    constructor(type,main=false) { // add if you add more make main the final one
+       this.key = "key("+key_incrementor+")"
+        key_incrementor+= 1
+        // body goes here
+        this.hero =  type
+        this.jason = main
+        if (main){
+
+        }
+        else{
+
+        }
+    }
+}
+
+
+
+
+
+
+
 console.log("classes loaded")
--- js/def.js
+++ js/def.js
@@ -1,1 +1,5 @@
+const start_btn = document.getElementById("start_btn")
+const menu_overlay = document.getElementById("menu_over")
+
+let key_incrementor = 0
 console.log("def loaded")
