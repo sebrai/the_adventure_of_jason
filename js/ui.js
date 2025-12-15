df863326de54d81077d25cbce97c64fce2f8931a
sebrai <sebbranden@gmail.com>
2025-12-15 11:30:14 +0100

    started base for animation

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,13 @@
+function untangle_que(animation_que) {
+    for (let num = 1; num < animation_que.length; num++) {
+        const element = animation_que[num];
+        const prev = animation_que[num -1];
+        prev.target.addEventListener("")
+    }
+}
+
+
+
+
+
 console.log("ui loaded")
