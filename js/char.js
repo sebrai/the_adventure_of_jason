ddb56a2809daccb25b43ee0c28a0a017e4bf006e
sebrai <sebbranden@gmail.com>
2025-12-16 11:24:38 +0100

    added in jason charachter

--- js/char.js
+++ js/char.js
@@ -1,1 +1,29 @@
+let jason = {
+    maxhp:100,
+    current:{ // describes anything that changes
+        hp:100,
+        dmg_buffs:[],
+        sheild_buffs:[],
+        items:{
+            potions:[],
+            perms:[],
+            equipables:{
+                armor:{
+                    helmet: null,
+                    chestplate: null,
+                    leggings: null,
+                    boots:null,
+                    gloves: null
+                },
+                mainhand:null,
+                sidearm:null,
+                sheild: null
+            },
+        },
+        aspect:0
+    },
+    sprites:[],// add sprites when created
+
+
+}
 console.log("char loaded")
