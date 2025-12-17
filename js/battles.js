040c251b40107b3e95a4d698b1bad6a1681ecc14
sebrai <sebbranden@gmail.com>
2025-12-17 13:54:09 +0100

    added multiple charachters to test styling

--- js/battles.js
+++ js/battles.js
@@ -1,2 +1,9 @@
-const main_player = new ally(jason);
+const main_player1 = new ally(jason);
+const main_player2 = new ally(jason);
+const main_player3 = new ally(jason);
+const main_player4 = new ally(jason);
+const test_enemy1 = new enemy(jason)
+const test_enemy2 = new enemy(jason)
+const test_enemy3 = new enemy(jason)
+const test_enemy4 = new enemy(jason)
 console.log("battles loaded")
