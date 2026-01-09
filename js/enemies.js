a0fcae5d73f649224590987240a5ebff274d98ce
sebrai <sebbranden@gmail.com>
2026-01-09 13:54:03 +0100

    created city guard

--- js/enemies.js
+++ js/enemies.js
@@ -3,13 +3,13 @@ let pirate = {
     name: "pirate",
     maxhp: 80,
     crit_chance: 5,
-    crit_mult:1.4,
+    crit_mult: 1.4,
     char_speed: 45,
-    speed_base:null,
+    speed_base: null,
     current: { // describes anything that changes
         hp: 80,
         crit_chance: 5,
-        speed:null,
+        speed: null,
         dmg_buffs: [],
         sheild_buffs: [],
         items: {
@@ -33,9 +33,9 @@ let pirate = {
     sprites: ["/temp/image.png"],// add sprites when created
     attacks: [
         {
-            dmg: 10,
+            dmg: 30,
             cost: 0,
-            index:0,
+            index: 0,
             range: "melee",
             type: "slash",
             status_eff: {
@@ -49,12 +49,12 @@ let pirate = {
             func: function (target, self) {
                 take_dmg(target, getdmg(this.dmg, self))
                 if (this.status_eff.apply) {
-                        apply_effect(target, this.status_eff.statusobjekt)
-                    }
+                    apply_effect(target, this.status_eff.statusobjekt)
+                }
             }
         }, {
             dmg: 40,
-            index:1,
+            index: 1,
             cost: 5,
             range: "melee",
             type: "slash",
@@ -72,43 +72,90 @@ let pirate = {
                     apply_effect(target, this.status_eff.statusobjekt)
                 }
             }
-         }
-         //, {
-        //     dmg: 0,
-        //     cost: 30,
-        //     index:2,
-        //     range: "self",
-        //     type: "magick",
-        //     target: "self",
-        //     status_eff: {
-        //         apply: true,
-        //         statusobjekt: {
-        //             power: 35,
-        //             duration: 4,
-        //             key: "locked in",
-        //         }
-        //     },
-        //     func: function (target, self) {
-        //         // target.hero.current.hp -= getdmg(0, self)
-                
-        //         // if (this.status_eff.apply) {
-        //         //     apply_effect(target, this.status_eff.statusobjekt)
-        //         // }
-        //     }
-        //  }
+        }
+
     ],
 
+}
 
-    ability: function (self, context = {}) {
-        if (context.end_wave) {
-            self.hero.crit_chance += 2
-            self.hero.current.crit_chance += 2
-        }
-        else if (context.on_status || context.get_hit){
-            self.hero.crit_chance += 1
-            self.hero.current.crit_chance += 1
-        }
+let city_guard = {
+    key: "city_guard",
+    name: "city guard",
+    maxhp: 90,
+    crit_chance: 10,
+    crit_mult: 1.2,
+    char_speed: 35,
+    speed_base: null,
+    current: { // describes anything that changes
+        hp: 90,
+        crit_chance: 10,
+        speed: null,
+        dmg_buffs: [],
+        sheild_buffs: [],
+        items: {
+            temps: [],
+            perms: [],
+            equipables: {
+                armor: {
+                    helmet: null,
+                    chestplate: null,
+                    leggings: null,
+                    boots: null,
+                    gloves: null
+                },
+                mainhand: null,
+                sidearm: null,
+                sheild: null
+            },
+        },
+        aspect: 0
     },
-        
-}  
+    sprites: ["/temp/guard.jpg"],// add sprites when created
+    attacks: [
+        {
+            dmg: 20,
+            index: 0,
+            range: "melee",
+            type: "peirce",
+            custom_critt: 35,
+            custom_critt_mult: 1.8,
+            status_eff: {
+                apply: false,
+                statusobjekt: {
+                    power: 0,
+                    duration: 0,
+                    key: "none",
+                }
+            },
+            func: function (target, self) {
+                take_dmg(target, getdmg(this.dmg, self))
+                if (this.status_eff.apply) {
+                    apply_effect(target, this.status_eff.statusobjekt)
+                }
+            }
+        }, {
+            dmg: 40,
+            index: 1,
+            range: "melee",
+            type: "slash",
+            status_eff: {
+                apply: false,
+                statusobjekt: {
+                    power: null,
+                    duration: null,
+                    key: null
+                }
+            },
+            func: function (target, self) {
+                take_dmg(target, getdmg(this.dmg, self))
+                if (this.status_eff.apply) {
+                    apply_effect(target, this.status_eff.statusobjekt)
+                }
+            }
+        }
+
+    ],
+
+}
+
 console.log("enemies.js loaded")
